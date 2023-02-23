083288536a55909ce7119d6988c3dff86213e807
lucas <gepeoverload@gmail.com>
2023-02-23 18:27:20 +0100

    implemented input handling tests

--- test/test_utility_handling.py
+++ test/test_utility_handling.py
@@ -11,22 +11,31 @@ def get_input(text):
     return input(text)
 
 
-def answer():
-    ans = get_input('enter yes or no')
-    if ans == 'yes':
-        return 'you entered yes'
-    if ans == 'no':
-        return 'you entered no'
-
-
 class TestUtilityHandling(unittest.TestCase):
 
-    @patch('test_utility_handling.get_input', return_value='yes')
-    def test_date_input_handling(self, input):
-        self.assertEqual(answer(), 'you entered yes')
+    @patch('builtins.input', return_value='12/12/2007')
+    def test_valid_date_input_handling(self, input):
+        self.assertEqual(util.handle_date_input(), '12/12/2007')
 
-    def test_amount_input_handling(self):
-        self.assertTrue(True)
+    @patch('builtins.input', side_effect=['32/12/2007', 'aaaaa', '12a12a1212', '12/12/2007'])
+    def test_invalid_date_input_handling(self, input):
+        self.assertEqual(util.handle_date_input(), '12/12/2007')
+
+    @patch('builtins.input', return_value='10')
+    def test_valid_amount_input_handling(self, input):
+        self.assertEqual(util.handle_amount_input(), 10)
+
+    @patch('builtins.input', return_value='99')
+    def test_valid_amount_input_handling_more_than_20(self, input):
+        self.assertEqual(util.handle_amount_input(), 1)
+
+    @patch('builtins.input', return_value='0')
+    def test_valid_amount_input_handling_less_than_1(self, input):
+        self.assertEqual(util.handle_amount_input(), 1)
+
+    @patch('builtins.input', side_effect=['-1', '&é&', 'AAAAAA', '5'])
+    def test_invalid_amount_input_handling(self, input):
+        self.assertEqual(util.handle_amount_input(), 5)
 
     def test_gender_input_handling(self):
         self.assertTrue(True)
