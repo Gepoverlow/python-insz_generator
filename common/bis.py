e61f9ad37ce5a6266fec6871c6849f471dcef388
lucas <gepeoverload@gmail.com>
2023-03-02 13:09:47 +0100

    changed importing way

--- common/bis.py
+++ common/bis.py
@@ -1,14 +1,4 @@
-from common.utility import \
-    daily_serial_number_generator, \
-    check_number_generator, \
-    date_format_generator,\
-    date_refactorer_bis,\
-    date_input_handler,\
-    amount_input_handler,\
-    is_gender_known_input_handler,\
-    is_dob_known_input_handler, \
-    fake_date_generator
-
+from common import utility as u
 from faker import Faker
 
 fake = Faker()
@@ -23,13 +13,13 @@ def bis_generator(is_gender_known: bool, is_birthday_known: bool, date: str, amo
 
     for i in range(amount):
         if generate_random_dates is True:
-            date: str = fake_date_generator()
+            date: str = u.fake_date_generator()
 
-        correct_date: str = date_refactorer_bis(date, is_gender_known, is_birthday_known)
+        correct_date: str = u.date_refactorer_bis(date, is_gender_known, is_birthday_known)
 
-        formatted_date: str = date_format_generator(correct_date)
-        daily_serial: str = daily_serial_number_generator('U')
-        check_number: str = check_number_generator(correct_date, daily_serial)
+        formatted_date: str = u.date_format_generator(correct_date)
+        daily_serial: str = u.daily_serial_number_generator('U')
+        check_number: str = u.check_number_generator(correct_date, daily_serial)
 
         bis_list.append(f'{formatted_date}{daily_serial}{check_number}')
 
@@ -39,10 +29,10 @@ def bis_generator(is_gender_known: bool, is_birthday_known: bool, date: str, amo
 def bis_printer():
     while True:
         try:
-            is_gender_known: bool = is_gender_known_input_handler()
-            is_dob_known: bool = is_dob_known_input_handler()
-            date: str = date_input_handler()
-            amount: int = amount_input_handler()
+            is_gender_known: bool = u.is_gender_known_input_handler()
+            is_dob_known: bool = u.is_dob_known_input_handler()
+            date: str = u.date_input_handler()
+            amount: int = u.amount_input_handler()
 
             bis_results: list[str] = bis_generator(is_gender_known, is_dob_known, date, amount)
 
