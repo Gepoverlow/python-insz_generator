d1c61c40cc68fff2ac7381049fb15319bc362322
lucas <gepeoverload@gmail.com>
2023-03-02 15:12:47 +0100

    improved error messages

--- api/validation/custom_types.py
+++ api/validation/custom_types.py
@@ -6,14 +6,14 @@ import hug
 @hug.type(extend=hug.types.number)
 def amount_type(value):
     if value < 1 or value > 20:
-        raise ValueError('Amount can be a minimum of 1 or a maximum of 20')
+        raise ValueError('Amount provided can be a minimum of 1 or a maximum of 20')
     return value
 
 
 @hug.type(extend=hug.types.text)
 def gender_type(value):
     if not u.is_gender_valid(value):
-        raise ValueError('Gender parameter not valid, please select between m, w or u')
+        raise ValueError('Gender provided is not valid, please select between m, w or u')
     return value
 
 
